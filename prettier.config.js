5a76b329822b6728a72371dbe55f38c5e1cb27a8
raifreire <rai.freire26@gmail.com>
2022-08-18 00:58:26 -0300

    add new routes

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,12 @@
 
 module.exports = {
-    singleQuote: true,
-    trailingComma: 'all',
-    arrowParens: 'avoid',
+    "prettier/prettier": [
+        "error",
+        {
+            "singleQuote": true,
+            "parser": "flow"
+        }
+    ]
+    //trailingComma: 'all',
+    //arrowParens: 'avoid',
 }
---
+++ src/model/Category.ts
@@ -0,0 +1,16 @@
+/* eslint-disable lines-between-class-members */
+import { v4 as uuidv4 } from 'uuid';
+
+class Category {
+    id?: string;
+    name!: string;
+    description!: string;
+    created_at!: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuidv4();
+        }
+    }
+}
+export default Category;
