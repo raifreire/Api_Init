222cf7dc4b06a85afb6793025de7048425043699
raifreire <rai.freire26@gmail.com>
2022-08-18 15:29:00 -0300

    add start solid

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -28,5 +28,10 @@ class CategoriesRepository {
     list(): Category[] {
         return this.categories;
     }
+
+    findByname(name: string): Category {
+        const category = this.categories.find((category) => category.name === name);
+        return category;
+    }
 }
 export { CategoriesRepository };
